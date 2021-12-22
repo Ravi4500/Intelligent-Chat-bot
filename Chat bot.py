07e0ba6de959787b6e505c93b522d0c0eaf5efe7
Ravi4500 <86669604+Ravi4500@users.noreply.github.com>
2021-12-22 19:01:15 +0530

    Update Chat bot.py

--- Chat bot.py
+++ Chat bot.py
@@ -4,55 +4,76 @@ import pywhatkit
 import datetime
 import wikipedia
 
+
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
 
-def talk(text):
+def speak(text):
     engine.say(text)
-    # engine.say('what can i do for you')
     engine.runAndWait()
 
 
-def take_command():
-    try:
-        with sr.Microphone() as source:
-            print("listening....")
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
+def take_input():
+    with sr.Microphone() as source:
+        print("I am listening....,speak out")
+        speak("I am listening....,speak out")
+        voice = listener.listen(source)
+        input = listener.recognize_google(voice)
+        input = input.lower()
 
-    except:
-        pass
-    return command
+    return input
+
+def message():
+    print("Please enter a mobile number")
+    print("Please enter a message which you want to send")
+    print("Please enter the time in hrs")
+    print("Please enter the number in minutes\n")
+    input1 = str(input())
+    input2 = str(input())
+    input3 = int(input())
+    input4 = int(input())
+    return [input1,input2,input3,input4]
 
 
-def run_alexa():
-    command = take_command()
+
+def run():
+    command = take_input()
     if 'play' in command:
         song = command.replace('play', '')
-        talk('playing' + song)
+        speak('playing' + song)
         pywhatkit.playonyt(song)
 
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
         print(time)
-        talk('current time is ' + time)
+        speak('current time is ' + time)
 
     elif 'who is' in command:
         person = command.replace('who is', '')
         info = wikipedia.summary(person, 1)
         print(info)
-        talk(info)
+        speak(info)
 
     elif 'search' in command:
         thing = command.replace('search', '')
+        speak("Give me a second,I will get you results")
         pywhatkit.search(thing)
 
+    elif 'send message' in command:
+        ans = message()
+        pywhatkit.sendwhatmsg("+91"+" "+ans[0],ans[1],ans[2],ans[3])
 
-run_alexa()
+    elif 'How are you' in command:
+        print(command)
+        speak("I am good, Thanks for asking and what about you")
+
+    elif 'what is' in command:
+        what = command.replace('what is', '')
+        speak("Give me a second,I will get you results")
+        pywhatkit.search(what)
+
+
+run()
